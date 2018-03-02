31abd7ad8f90147398e02b18094844736ad5e512
AllisonCook27 <allison.cook@ocsbstudent.ca>
2018-03-02 14:29:44 -0500

    all

--- RandomNumberAllisonC/RandomNumberForm.Designer.cs
+++ RandomNumberAllisonC/RandomNumberForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace RandomNumberAllisonC
 {
-    partial class Form1
+    partial class frmRandomNumber
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,136 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnGuess = new System.Windows.Forms.Button();
+            this.lblWrong = new System.Windows.Forms.Label();
+            this.lblEnter = new System.Windows.Forms.Label();
+            this.lblRight = new System.Windows.Forms.Label();
+            this.mnuMenu = new System.Windows.Forms.MenuStrip();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.picCheck = new System.Windows.Forms.PictureBox();
+            this.mniFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuMenu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picCheck)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGuess
+            // 
+            this.btnGuess.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuess.Location = new System.Drawing.Point(158, 117);
+            this.btnGuess.Name = "btnGuess";
+            this.btnGuess.Size = new System.Drawing.Size(100, 35);
+            this.btnGuess.TabIndex = 0;
+            this.btnGuess.Text = "Guess";
+            this.btnGuess.UseVisualStyleBackColor = true;
+            this.btnGuess.Click += new System.EventHandler(this.btnGuess_Click);
+            // 
+            // lblWrong
+            // 
+            this.lblWrong.AutoSize = true;
+            this.lblWrong.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWrong.Location = new System.Drawing.Point(21, 181);
+            this.lblWrong.Name = "lblWrong";
+            this.lblWrong.Size = new System.Drawing.Size(144, 20);
+            this.lblWrong.TabIndex = 1;
+            this.lblWrong.Text = "Sorry, guess agian.";
+            // 
+            // lblEnter
+            // 
+            this.lblEnter.AutoSize = true;
+            this.lblEnter.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEnter.Location = new System.Drawing.Point(21, 51);
+            this.lblEnter.Name = "lblEnter";
+            this.lblEnter.Size = new System.Drawing.Size(262, 20);
+            this.lblEnter.TabIndex = 2;
+            this.lblEnter.Text = "Guess a number between 1 and 10:";
+            // 
+            // lblRight
+            // 
+            this.lblRight.AutoSize = true;
+            this.lblRight.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRight.Location = new System.Drawing.Point(21, 181);
+            this.lblRight.Name = "lblRight";
+            this.lblRight.Size = new System.Drawing.Size(154, 20);
+            this.lblRight.TabIndex = 3;
+            this.lblRight.Text = "You guessed it right!";
+            // 
+            // mnuMenu
+            // 
+            this.mnuMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniFile});
+            this.mnuMenu.Location = new System.Drawing.Point(0, 0);
+            this.mnuMenu.Name = "mnuMenu";
+            this.mnuMenu.Size = new System.Drawing.Size(416, 24);
+            this.mnuMenu.TabIndex = 4;
+            this.mnuMenu.Text = "menuStrip1";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(304, 51);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(100, 20);
+            this.txtInput.TabIndex = 5;
+            // 
+            // picCheck
+            // 
+            this.picCheck.Location = new System.Drawing.Point(273, 160);
+            this.picCheck.Name = "picCheck";
+            this.picCheck.Size = new System.Drawing.Size(113, 78);
+            this.picCheck.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picCheck.TabIndex = 6;
+            this.picCheck.TabStop = false;
+            // 
+            // mniFile
+            // 
+            this.mniFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniExit});
+            this.mniFile.Name = "mniFile";
+            this.mniFile.Size = new System.Drawing.Size(37, 20);
+            this.mniFile.Text = "File";
+            // 
+            // mniExit
+            // 
+            this.mniExit.Name = "mniExit";
+            this.mniExit.Size = new System.Drawing.Size(152, 22);
+            this.mniExit.Text = "Exit";
+            this.mniExit.Click += new System.EventHandler(this.mniExit_Click);
+            // 
+            // frmRandomNumber
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.Salmon;
+            this.ClientSize = new System.Drawing.Size(416, 261);
+            this.Controls.Add(this.picCheck);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.lblRight);
+            this.Controls.Add(this.lblEnter);
+            this.Controls.Add(this.lblWrong);
+            this.Controls.Add(this.btnGuess);
+            this.Controls.Add(this.mnuMenu);
+            this.MainMenuStrip = this.mnuMenu;
+            this.Name = "frmRandomNumber";
+            this.Text = "Random Number by Allison C";
+            this.mnuMenu.ResumeLayout(false);
+            this.mnuMenu.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picCheck)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnGuess;
+        private System.Windows.Forms.Label lblWrong;
+        private System.Windows.Forms.Label lblEnter;
+        private System.Windows.Forms.Label lblRight;
+        private System.Windows.Forms.MenuStrip mnuMenu;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.PictureBox picCheck;
+        private System.Windows.Forms.ToolStripMenuItem mniFile;
+        private System.Windows.Forms.ToolStripMenuItem mniExit;
     }
 }
 
--- RandomNumberAllisonC/RandomNumberForm.cs
+++ RandomNumberAllisonC/RandomNumberForm.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*
+     * Created by: Allison Cook
+     * Created on: 2 March, 2018
+     * Created for: ICS3U Programming
+     * Daily Assignment – Day #16 - Random Number
+     * This program asks the user to guess a number form 1-10 and see if the guessed the right number
+    */
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,11 +17,65 @@ using System.Windows.Forms;
 
 namespace RandomNumberAllisonC
 {
-    public partial class Form1 : Form
+    public partial class frmRandomNumber : Form
     {
-        public Form1()
+        //variable
+        int userNumber;
+        public frmRandomNumber()
         {
             InitializeComponent();
+            //hide text before it starts
+            lblRight.Hide();
+            lblWrong.Hide();
+        }
+
+        private void mniExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnGuess_Click(object sender, EventArgs e)
+        {
+            //constant numbers
+            const int MAX = 10;
+            const int MIN = 1;
+
+            //calling random number 
+            Random randomNumberGenerator = new Random();
+
+            //get random number
+            int randomNumber = randomNumberGenerator.Next(MIN, MAX + 1);
+            //hide text if using agian without starting up
+            lblRight.Hide();
+            lblWrong.Hide();
+
+           //getting random number
+
+
+            try
+            {
+                //Try to set it as a number
+                userNumber = Convert.ToInt16(txtInput.Text);
+            }
+            catch
+            {
+                //tell the user they need to input a number
+                MessageBox.Show("Please input a number");
+            }
+
+            if (userNumber == randomNumber)
+            {
+                //show the user they got it right
+                lblRight.Show();
+                this.picCheck.Image = Properties.Resources.checkmark;
+            }
+            else
+            {
+                //show the user they got it wrong
+                lblWrong.Show();
+                this.picCheck.Image = Properties.Resources.red_x;
+
+            }
         }
     }
 }
